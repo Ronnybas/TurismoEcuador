db79791b1dc87763561d1a898a229ca8942f3d63
Ronny <rabastidas@espe.edu.ec>
2025-11-12 02:38:19 -0800

    Version con Formulario

---
+++ holamundo.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>¡Hola, mundo!</h1>";
+$i=0; 
+echo "<table border='1'>";
+echo "<tr><td> Numeros del 1 al 10 </td></tr>";
+for ($i=1;  $i<=10;  $i++) {
+    echo "<tr><td align='center'>".$i . "</td></tr>";
+}
+?>
